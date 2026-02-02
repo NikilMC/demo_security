5624591f03d364de9d0390b0bb0acce3e8bd8a3e
Nikil Raagav <ssnikilraagav1201@gmail.com>
2026-02-03 03:01:40 +0530

    demo-security first commit

---
+++ src/main/java/com/springboot/demosecurity/controller/DemoController.java
@@ -0,0 +1,28 @@
+package com.springboot.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+    @GetMapping("/")
+    public String showHome(){
+        return "home";
+    }
+    @GetMapping("/leaders")
+    public String showLeaders(){
+        return "leaders";
+    }
+    @GetMapping("/systems")
+    public String showSystems(){
+        return "systems";
+    }
+    @GetMapping("/showMyLoginPage")
+    public String showMyLoginPage(){
+        return "plain-login";
+    }
+    @GetMapping("/accessdenied")
+    public String accessDeniedPage(){
+        return "accessdenied";
+    }
+}
